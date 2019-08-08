551be5950e956b78676a239146a5a0ed22e9052c
Bryan Anthony Garcia <bryan@xam-consulting.com>
2019-08-08 23:15:16 +1000

    Add project files.

---
+++ SampleXamApp/SampleXamApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleXamApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<string> messageList = new ObservableCollection<string>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            listView.ItemsSource = messageList;
+        }
+
+        private async void SendButton_Clicked(object sender, EventArgs e)
+        {
+            var message = messageEntry.Text;
+            await DisplayAlert(Device.RuntimePlatform == Device.iOS ? "Message Sent thru iOS" : "Message Sent thru Android", $"Message: {message}", "OK");
+            messageList.Add(message);
+        }
+    }
+}
